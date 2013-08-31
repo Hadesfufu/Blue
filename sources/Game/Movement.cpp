8d3d07861362ea28f29d4e4a972731ec3fa7ec37
HadesFufu <yohan.masson@neuf.fr>
2013-08-31 23:05:51 +0200

    Commit Test
    Dabadee

--- sources/Game/Movement.cpp
+++ sources/Game/Movement.cpp
@@ -5,42 +5,95 @@
 
 #include "Physics/Physics.h"
 
-const float SMovable::MaxVelocity = 100.f;
+const float CMovementSystem::MaxVelocity = 5.0f;
+const float CMovementSystem::Gravity = 0.75f;
+const float CMovementSystem::SpeedFactor = 64.0f;
 
 tSystemID CMovementSystem::id = -1;
 
-SMovable::SMovable(const sf::Vector2f& vel)
-: velocity(vel)
-{
+bool CMovementSystem::jump( tObjectID id ) {
+	if( mCanJump == false ) return false;
 
+	auto found = mComponents.find( id );
+	found->second->y = -MaxVelocity * 2.5f;
+
+	return true;
 }
 
-void SMovable::limitVelocity()
-{
-	velocity.x = std::min(velocity.x, MaxVelocity);
-	velocity.y = std::min(velocity.y, MaxVelocity);
+void CMovementSystem::setVelocityX( tObjectID id, float velX ) {
+	auto found = mComponents.find( id );
+	found->second->x = std::min( velX, MaxVelocity );
 }
 
-void CMovementSystem::scaleVelocity(tObjectID id, const sf::Vector2f& vel)
-{
-	auto found = mComponents.find(id);
-	found->second->velocity += vel;
+void CMovementSystem::setVelocityY( tObjectID id, float velY ) {
+	auto found = mComponents.find( id );
+	found->second->y = std::min( velY, MaxVelocity );
 }
 
-void CMovementSystem::onUpdate(const sf::Time dt)
-{
-	for(auto it = mComponents.begin() ; it != mComponents.end() ; ++it)
-	{
-		sf::Vector2f tmp = CSystemManager::instance()->getSystem<CPositionSystem>()->getPosition(it->first);
 
-		it->second->velocity.y += 9.81 * 2 * dt.asSeconds();
-		it->second->limitVelocity();
+void CMovementSystem::onMove( tObjectID id, const sf::Time& dt, float moveX, float moveY ) {
+	if( moveX == 0 && moveY == 0 ) return;
 
-		sf::Vector2f newPosition = tmp + it->second->velocity * dt.asSeconds();
+	mCanJump = false;
 
-		if(CSystemManager::instance()->getSystem<CPhysicsSystem>()->isColliding(it->first))
-			it->second->velocity = sf::Vector2f(0.0f, 0.0f);
+	auto found = mComponents.find( id );
+	sf::Vector2f& pos = CSystemManager::instance()->getSystem<CPositionSystem>()->getPosition( found->first );
+
+	float newX = 0;
+	float newY = 0;
+
+	moveX *= dt.asSeconds() * SpeedFactor;
+	moveY *= dt.asSeconds() * SpeedFactor;
+
+	if( moveX != 0 ) {
+		if( moveX >= 0 )	newX = dt.asSeconds() * SpeedFactor;
+		else				newX = -dt.asSeconds() * SpeedFactor;
+	}
+
+	if( moveY != 0 ) {
+		if( moveY >= 0 )	newY = dt.asSeconds() * SpeedFactor;
+		else				newY = -dt.asSeconds() * SpeedFactor;
+	}
+
+	while( true ) {
+		if( CSystemManager::instance()->getSystem<CPhysicsSystem>()->posValid( found->first, pos.x + newX, pos.y ) )
+			pos.x += newX;
 		else
-			CSystemManager::instance()->getSystem<CPositionSystem>()->setComponent(it->first, newPosition);
+			found->second->x = 0;
+
+		if( CSystemManager::instance()->getSystem<CPhysicsSystem>()->posValid( found->first, pos.x, pos.y + newY ) )
+			pos.y += newY;
+		else {
+			if( moveY > 0 ) mCanJump = true;
+			found->second->y = 0;
+		}
+
+		moveX += -newX;
+		moveY += -newY;
+
+		if( newX > 0 && moveX <= 0 ) newX = 0;
+		if( newX < 0 && moveX >= 0 ) newX = 0;
+
+		if( newY > 0 && moveY <= 0 ) newY = 0;
+		if( newY < 0 && moveY >= 0 ) newY = 0;
+		
+		if( moveX == 0 ) newX = 0;
+		if( moveY == 0 ) newY = 0;
+
+		if( moveX == 0 && moveY == 0 ) break;
+		if( newX  == 0 && newY  == 0 ) break;
+	}
+}
+
+void CMovementSystem::onUpdate( const sf::Time dt ) {
+	for( auto it = mComponents.begin() ; it != mComponents.end() ; ++it ) {
+		// apply gravity
+		it->second->y += Gravity * dt.asSeconds() * SpeedFactor;
+
+		// limit velocity
+		it->second->x = std::min( it->second->x, MaxVelocity );
+		it->second->y = std::min( it->second->y, MaxVelocity );
+
+		onMove( it->first, dt, it->second->x, it->second->y );
 	}
 }
